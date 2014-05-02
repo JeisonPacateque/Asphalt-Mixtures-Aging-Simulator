e8e350adcf381bd133050f05f86a87f5b1c5949a
hibikimsp <hibikimsp@gmail.com>
2014-05-02 16:29:02 -0500

    Corregido el sobre-uso de memoria, script seguro para ejecutar

--- archivos.py
+++ archivos.py
@@ -5,17 +5,20 @@ Created on 2/05/2014
 '''
 import os
 import dicom
+import matplotlib.pyplot as plt
+
 
 archivos=[] #Arreglo de archivos
 
 for dirname, dirnames, filenames in os.walk('/home/santiago/Documentos/Pruebas Python/66719/6/'):
-    # print path to all subdirectories first.
+    # print path to all subdirectories first.s
     for subdirname in dirnames:
         print os.path.join(dirname, subdirname)
 
     # print path to all filenames.
     for filename in filenames:
-        ruta_archivo=os.path.join(dirname, filename)
-        print ruta_archivo
-        #print os.path.join(filename)
-        archivos.append(dicom.read_file(ruta_archivo))
+        ruta_archivo=os.path.join(dirname, filename)    #Crea la ruta del archivo
+        print ruta_archivo                              #Muesta la ruta de cada archivo por consola
+        temporal=dicom.read_file(ruta_archivo)          #Variable temporal para poder usar el metodo pixel_array
+        imagen=temporal.pixel_array                     #Tranformacion de imagen dicom a arreglo numpy
+        archivos.append(imagen)                         #Agrega la imagen a memoria
