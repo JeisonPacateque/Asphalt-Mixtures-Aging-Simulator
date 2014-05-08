a70a3480baff2f20ab4f7d3bc5466d23e1486577
hibikimsp <hibikimsp@gmail.com>
2014-05-07 20:50:09 -0500

    Mejorada visualización, renderiza utilizando el objeto FileLoader

--- Qt_window.py
+++ Qt_window.py
@@ -3,8 +3,7 @@ import sys
 from PyQt4 import QtGui, QtCore
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-import dicom
-import file_loader
+from file_loader import FileLoader
 
 
 class MyMplCanvas(FigureCanvas):
@@ -28,40 +27,35 @@ class MyMplCanvas(FigureCanvas):
     def compute_initial_figure(self):
         pass
 
-
-class MyStaticMplCanvas(MyMplCanvas):
-    #Load the static plot
-    def compute_initial_figure(self):
-        ds = dicom.read_file('/home/santiago/Documentos/Pruebas Python/PruebasGraficas/00490278')
-        arreglo = ds.pixel_array
-        fixed = arreglo[35:485, 35:485]
-        self.axes.imshow(fixed, cmap='seismic')
-
+loader=FileLoader() #Creates the FileLoader object
+collection=loader.get_collection() #Gets the collection of images
 
 class MyDynamicMplCanvas(MyMplCanvas):
     #Load the dinamic plot    
-    global miGlobal
+    global index
+    loader.load_path('/home/santiago/Documentos/Pruebas Python/66719/6/') #Load the path
+    
     def __init__(self, *args, **kwargs):
         MyMplCanvas.__init__(self, *args, **kwargs)
-        global miGlobal
-        miGlobal = 0
-        timer = QtCore.QTimer(self)
+        global index
+        index = 0                       #Set the initial index
+        timer = QtCore.QTimer(self)     #Timer intended to update the image
         QtCore.QObject.connect(timer, QtCore.SIGNAL("timeout()"), self.update_figure)
-        timer.start(500)
+        timer.start(100)                #Set the update time
 
     def compute_initial_figure(self):
-        self.axes.imshow(file_loader.coleccion_imagenes[0], cmap='seismic')
+        self.axes.imshow(collection[0], cmap='seismic') #Set the initial image
 
     def update_figure(self):
         #Read and plot all the images stored in the image list
-        global miGlobal
-        if miGlobal != len(file_loader.coleccion_imagenes) : #Conditional to restart the loop
-            self.axes.imshow(file_loader.coleccion_imagenes[miGlobal], cmap='seismic')
-            print "Imagen numero: ",miGlobal
-            miGlobal += 1
+        global index
+        if index != len(collection) : #Conditional to restart the loop  
+            self.axes.imshow(collection[index], cmap='seismic')
+            print "Imagen numero: ",index
+            index += 1
             self.draw()
         else:
-            miGlobal=0
+            index=0
 
 class ApplicationWindow(QtGui.QMainWindow):
     def __init__(self):
@@ -83,7 +77,6 @@ class ApplicationWindow(QtGui.QMainWindow):
         self.main_widget = QtGui.QWidget(self)
 
         l = QtGui.QGridLayout(self.main_widget)
-        sc = MyStaticMplCanvas(self.main_widget, width=5, height=4, dpi=100)
         dc = MyDynamicMplCanvas(self.main_widget, width=5, height=4, dpi=100)
         #l.addWidget(sc)
         l.addWidget(dc)
@@ -101,14 +94,18 @@ class ApplicationWindow(QtGui.QMainWindow):
         self.fileQuit()
 
     def about(self):
-        QtGui.QMessageBox.about(self, "About",
-        """Primer prueba del entorno Qt utilizando tipos de datos de 
-    MatPlotLib renderizados con la libreriagrafica Qt en Python""")
+        QtGui.QMessageBox.about(self, "Acerca de",
+        """Primera prueba del entorno Qt utilizando tipos de datos
+MatPlotLib renderizados con la libreria grafica Qt en Python
+
+Desarrollado por:
+Jeison Pacateque
+Santiago Puerto""")
 
 qApp = QtGui.QApplication(sys.argv)
 
 aw = ApplicationWindow()
-aw.setWindowTitle("Entorno Qt")
+aw.setWindowTitle("Visor de muestra DICOM")
 aw.show()
 sys.exit(qApp.exec_())
 #qApp.exec_()
