73af63a4ff616a1b706d371627ee379b75ddc257
hibikimsp <hibikimsp@gmail.com>
2014-05-08 01:52:31 -0500

    Load time measurement implemented (console)

--- file_loader.py
+++ file_loader.py
@@ -5,32 +5,37 @@ Created on 2/05/2014
 '''
 import os
 import dicom
+import time
 
 coleccion_imagenes = [] #Image list
 
 class FileLoader:
     
     def load_path(self, path):
+        
+        start_time=time.time()  #Measures file loading time       
+        print "Loading dicom files..."
 
         for dirname, dirnames, filenames in os.walk(path):
-#        '/home/santiago/Documentos/Pruebas Python/66719/6/'
+
             # print path to all subdirectories first.s
             for subdirname in dirnames:
                 print os.path.join(dirname, subdirname)
             
-            filenames.sort()    #Sort all the files by name
+            filenames.sort()    #Sort files by name
         
-            # print path to all filenames.
+            # join the path with all filenames.
             for filename in filenames:
                 ruta_archivo=os.path.join(dirname, filename)        #Set the path file
-                print ruta_archivo                                  #Print file path
+                #print ruta_archivo                                  #Print file path
                 temporal = dicom.read_file(ruta_archivo)            #Read the file as DICOM image
                 imagen = temporal.pixel_array                       #Transform DICOM image as numpy array
                 fixed = imagen[35:485, 35:485]                      #Cut image to fit plot
                 coleccion_imagenes.append(fixed)                    #Add current image to a list
                 
         num_archivos=len(coleccion_imagenes)        
-        print "Finalizada la carga de ", num_archivos, " archivos."
+        end_time=time.time()    #Get the time when method ends
+        print num_archivos, "dicom files loaded in ", str(end_time - start_time), " seconds."
         
     def get_collection(self):
         return coleccion_imagenes   #Access method to the loaded images
