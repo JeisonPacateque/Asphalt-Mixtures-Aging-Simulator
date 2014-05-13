bdfb2580236fb5df7be4310066081bc467609b26
hibikimsp <hibikimsp@gmail.com>
2014-05-13 10:47:48 -0500

    Translated to english, added animation control funtionalities, removed global variables in order to have an OOP application.

--- Qt_window.py
+++ Qt_window.py
@@ -1,33 +1,39 @@
 from __future__ import unicode_literals
 import sys
-import numpy as np
 import matplotlib.image as mpimg
 from PyQt4 import QtGui, QtCore
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-from file_loader import FileLoader, FileLoaderNPY
+from file_loader import FileLoader
+from _markerlib import __init__
 
 
 class ApplicationWindow(QtGui.QMainWindow):
-    
-    global collection
+    collection = [] #Class attribute
+    timer = QtCore.QTimer()     #Timer intended to update the image
 
     def __init__(self):
 
         QtGui.QMainWindow.__init__(self)
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
-        self.setWindowTitle("Ventana Principal")
 
-        self.file_menu = QtGui.QMenu('&Archivo', self)
-        self.file_menu.addAction('&Salir', self.fileQuit,
+        self.file_menu = QtGui.QMenu('&File', self)
+        self.file_menu.addAction('&Exit', self.fileQuit,
                                  QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
         self.menuBar().addMenu(self.file_menu)
+        
+        self.sample_menu = QtGui.QMenu('&Sample', self)
+        self.sample_menu.addAction('&Resume', self.resume_animation,
+                                   QtCore.Qt.CTRL + QtCore.Qt.Key_R)
+        self.sample_menu.addAction('&Pause', self.stop_animation,
+                                   QtCore.Qt.CTRL + QtCore.Qt.Key_P)
+        self.menuBar().addMenu(self.sample_menu)
 
-        self.help_menu = QtGui.QMenu('&Ayuda', self)
+        self.help_menu = QtGui.QMenu('&Help', self)
         self.menuBar().addSeparator()
         self.menuBar().addMenu(self.help_menu)
 
-        self.help_menu.addAction('&Acerca de...', self.about)
+        self.help_menu.addAction('&About...', self.about)
 
         open_button = QtGui.QPushButton('Choose work path', self)
         open_button.clicked[bool].connect(self.open_path) #Button listener
@@ -48,6 +54,7 @@ class ApplicationWindow(QtGui.QMainWindow):
         self.setCentralWidget(self.main_widget)
         
     def open_path(self, pressed):
+
         chosen_path = QtGui.QFileDialog.getExistingDirectory(None, 
                                                          'Open working directory', 
                                 '/home/santiago/Documentos/Pruebas Python/66719/', 
@@ -57,17 +64,20 @@ class ApplicationWindow(QtGui.QMainWindow):
         print "Load files from: " + path
         self.folder_path.setText(path)
 
-        loader=FileLoader() #Creates the FileLoader object
-        loader.load_path(path) #Load the path
-        global collection
-        collection = loader.get_collection()
+        #Modifiying the class attribute to get the loaded collection of images
+        self.__class__.collection = FileLoader().load_path(path)
 
-        timer = QtCore.QTimer(self)     #Timer intended to update the image
-        QtCore.QObject.connect(timer, QtCore.SIGNAL("timeout()"), self.dc.update_figure)
-        timer.start(100)                #Set the update time
+        QtCore.QObject.connect(self.__class__.timer, QtCore.SIGNAL("timeout()"), self.dc.update_figure)
+        self.__class__.timer.start(100)                #Set the update time
 
+    def stop_animation(self):
+        self.__class__.timer.stop()
+        
+    def resume_animation(self):
+        self.__class__.timer.start(100)
+        
     def update_staus(self, message):
-        self.statusBar().showMessage("Muestra: "+message)
+        self.statusBar().showMessage("Sample: "+message)
 
     def fileQuit(self):
         self.close()
@@ -75,15 +85,12 @@ class ApplicationWindow(QtGui.QMainWindow):
     def closeEvent(self, ce):
         self.fileQuit()
 
-    def contador(self):
-        return self
-
     def about(self):
-        QtGui.QMessageBox.about(self, "Acerca de",
-        """Primera prueba del entorno Qt utilizando tipos de datos
-MatPlotLib renderizados con la libreria grafica Qt en Python
+        QtGui.QMessageBox.about(self, "About",
+        """First attempt to inplement a Qt enviroment rendering 
+MatPlotLib figures created from DICOM files
 
-        Desarrollado por:
+        Developed by:
         Jeison Pacateque
         Santiago Puerto""")
         
@@ -92,8 +99,7 @@ class Canvas(FigureCanvas):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
-        # We want the axes cleared every time plot() is called
-        self.axes.hold(False)
+        self.axes.hold(False) #We want the axes cleared every time plot() is called
         initial = mpimg.imread('/home/santiago/Proyecto-de-Grado-Codes/images/python.png')
         self.axes.imshow(initial)   #Show an initial image for the app
         FigureCanvas.__init__(self, fig)
@@ -108,19 +114,17 @@ class Canvas(FigureCanvas):
 
 class MyDynamicMplCanvas(Canvas):
     # Load the dinamic plot
-    global index
     index = 0
     def update_figure(self):
         #Read and plot all the images stored in the image list
-        global index        #Call the index counter
-        global collection   #Call the collection from loader
-        if index != len(collection) : #Conditional to restart the loop
-            self.axes.imshow(collection[index], cmap='seismic')
-            aw.update_staus(str(index)) #Show in status bar the current index
-            index += 1
+        collection = ApplicationWindow.collection
+        if self.__class__.index != len(collection) : #Conditional to restart the loop
+            self.axes.imshow(collection[self.__class__.index], cmap='seismic')
+            aw.update_staus(str(self.__class__.index)) #Show in status bar the current index
+            self.__class__.index += 1
             self.draw()
         else:
-            index=0
+            self.__class__.index=0
 
 #------------------------------------------------------------------------
 
@@ -128,7 +132,7 @@ class MyDynamicMplCanvas(Canvas):
 qApp = QtGui.QApplication(sys.argv)
 
 aw = ApplicationWindow()
-aw.setWindowTitle("Visor de muestra DICOM")
+aw.setWindowTitle("DICOM Samples Viewer")
 aw.show()
 sys.exit(qApp.exec_())
 #qApp.exec_()
