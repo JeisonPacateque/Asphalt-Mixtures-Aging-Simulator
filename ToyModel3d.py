36bbcfeceddb73ee4eaab6af14fc21674ff17ebd
hibikimsp <hibikimsp@gmail.com>
2014-06-08 19:07:40 -0500

    Mayavi features hardcoded to have two slice viewers and the axis reference

--- ToyModel3d.py
+++ ToyModel3d.py
@@ -11,13 +11,9 @@ from mayavi import mlab
 
 def ToyModel3d(sample):
     src = mlab.pipeline.scalar_field(sample)
+    
     mlab.pipeline.iso_surface(src, contours=[1], opacity=0.4)
-#    mlab.pipeline.iso_surface(src, opacity=0.4)
-    #
-    mlab.pipeline.image_plane_widget(src,
-                                plane_orientation='x_axes',
-                                slice_index=10,
-                            )
-    #fig=mlab.pipeline.iso_surface(src, color=(1.0,1.0,0.0), contours=[0.1, ], opacity=0.3)
-    #mlab.pipeline.iso_surface(src, color=(1.0,1.0,1.0),contours=[-2, ], opacity=0.3)
+    mlab.pipeline.image_plane_widget(src, plane_orientation='y_axes', slice_index=10)
+    mlab.pipeline.scalar_cut_plane(src)
+    mlab.orientation_axes()
     mlab.show()
