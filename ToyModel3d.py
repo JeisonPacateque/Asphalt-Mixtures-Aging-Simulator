9b312b02e800d1683415574f74ca8924360299fb
hibikimsp <hibikimsp@gmail.com>
2014-06-10 21:57:46 -0500

    Parameters added, now the colormap and the number of colors are hardcoded to differentiate the invalid empty pixels

--- ToyModel3d.py
+++ ToyModel3d.py
@@ -6,14 +6,24 @@ Created on Tue May 27 09:10:49 2014
 """
 
 from mayavi import mlab
-#from tvtk.api import tvtk
 
 
 def ToyModel3d(sample):
-    src = mlab.pipeline.scalar_field(sample)
+    src = mlab.pipeline.scalar_field(sample)  
+    inverse_lut = False
+    colors = 5
     
-    mlab.pipeline.iso_surface(src, contours=[1], opacity=0.4)
-    mlab.pipeline.image_plane_widget(src, plane_orientation='y_axes', slice_index=10)
-    mlab.pipeline.scalar_cut_plane(src)
+    iso = mlab.pipeline.iso_surface(src, contours=[1], opacity=0.4, colormap = 'blue-red')
+    iso.module_manager.scalar_lut_manager.reverse_lut = inverse_lut
+    iso.module_manager.scalar_lut_manager.number_of_colors = colors
+    
+    ipw = mlab.pipeline.image_plane_widget(src, plane_orientation='y_axes', slice_index=10, colormap = 'blue-red')
+    ipw.module_manager.scalar_lut_manager.reverse_lut = inverse_lut
+    ipw.module_manager.scalar_lut_manager.number_of_colors = colors
+    
+    scp = mlab.pipeline.scalar_cut_plane(src, colormap = 'blue-red')
+    scp.module_manager.scalar_lut_manager.reverse_lut = inverse_lut
+    scp.module_manager.scalar_lut_manager.number_of_colors = colors
+    
     mlab.orientation_axes()
     mlab.show()
