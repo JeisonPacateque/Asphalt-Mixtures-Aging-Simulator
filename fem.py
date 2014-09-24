d051a3e3b53d4f0411c102454387edca4e075811
hibikimsp <hibikimsp@gmail.com>
2014-09-24 15:41:04 -0500

    Mesh cubic generator (cylinder commented)

--- fem.py
+++ fem.py
@@ -5,8 +5,7 @@ Created on Fri Aug  1 14:37:30 2014
 @author: santiago
 """
 
-from tvtk.api import write_data, tvtk
-from sfepy.mesh.mesh_generators import gen_block_mesh
+import sfepy.mesh.mesh_generators as generator
 import numpy as np
 import time
 
@@ -16,38 +15,41 @@ class VectorWriter(object):
     def __init__(self):
         pass
     
-#    def toymodel_to_vector(self, collection):
-#        """This method transform a segmented Toy Model object (collection) and returns
-#        the model equivalent Mesh"""
-#
-#        size=collection.shape
-#        mesh = tvtk.ImageData(spacing=(size), origin=(0, 0, 0))
-#        mesh.point_data.scalars = collection.ravel(order='F')
-#        mesh.point_data.scalars.name = 'scalars'
-#        mesh.dimensions = collection.shape    
+#    def save_vtk(self, collection, filename):
+#        """This method saves the Toy Model object to a VTK manipulable file"""
 #        
-#        return mesh
-        
+#        start_time = time.time()  # Measures file loading time
+#        
+#        dims = collection.shape
+#        x_lenght = dims[1]/2
+#        y_lenght = dims[2]/2
+#        z_lenght = dims[0]
+#        
+#        dimensiones = (0, 0, x_lenght, y_lenght, dims[0])
+#        forma = (x_lenght, y_lenght, dims[0])
+#        centro = (x_lenght/2, y_lenght/2, z_lenght/2)
+#        
+#        mesh = generator.gen_cylinder_mesh(dimensiones, forma, centro, axis='z', force_hollow=False, non_uniform=False ,name='ToyModel', verbose=True )
+#        
+#        mesh.write(filename)
+#        end_time = time.time()  # Get the time when method ends
+#        
+#        print "Done writting VTK file at", filename, str(end_time - start_time), " seconds."
+       
+         
     def save_vtk(self, collection, filename):
-        """This method saves a vectorrized Toy Model object to a VTK manipulable file"""
+        """This method saves the Toy Model object to a VTK manipulable file"""
         
         start_time = time.time()  # Measures file loading time
-        dimensions = collection.shape
-        mesh = gen_block_mesh((1, 1, 1), dimensions, (0, 0, 0), name='ToyModel', verbose=True)
-        mesh.mat_ids
+        
+        #dimensions = collection.shape
+        dimensions = np.array((23, 99, 100))
+        #print dimensions
+    
+        mesh = generator.gen_block_mesh(dimensions, dimensions, (0, 0, 0), name='ToyModel', verbose=True)
+        
         mesh.write(filename)
         end_time = time.time()  # Get the time when method ends
         
         print "Done writting VTK file at", filename, str(end_time - start_time), " seconds."
-        
-    def create_test_slice(self, toymodel):
-        """Test method to perform a FEM mechanical simulation """
-        print "Creating one slice"
-        test_slice = toymodel[:,50,:] #Vertical slice
-        #size = test_slice.shape
-        polys = np.array([[0,1,3], [0,3,2], [1,2,3], [0,2,1]])
-        test_mesh = tvtk.PolyData(points=test_slice, polys=polys)
-        test_mesh.point_data.scalars = test_slice
-        test_mesh.point_data.scalars.name = 'Temperature'
-        write_data(test_mesh, '/home/santiago/tajada.vtk')
-        print "Done creating one slice"
+       
