863a9148f7aad545edc5cc148f4988181a0e390c
hibikimsp <hibikimsp@gmail.com>
2015-01-24 19:07:30 -0500

    thermal model now more O-OP!

--- thermal_model.py
+++ thermal_model.py
@@ -10,90 +10,38 @@ import numpy as np
 import scipy as sp
 from PyQt4 import QtGui, QtCore
 from pylab import cm
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
 
 
 class ThermalModel(QtGui.QDialog):
-    def __init__(self, Material, parent=None):
+    def __init__(self, materials, parent=None):
         super(ThermalModel, self).__init__(parent)
-
-        self.material = Material
-        self.sample = self.material.loadVerticalSlice().transpose()
-        self.thermicalConstantsMatrix = self.material.thermicalConstantsMatrix.transpose() # matriz de thermicalConstantsMatrixantes de conductividad
-        self.ui = np.zeros((self.sample.shape)) # matriz inicial de temp
-        self.u = np.zeros((self.sample.shape)) # matriz inicial de temp
-
-
-    def runSimulationAnimated(self):
-        print "Running thermical simulation..."
-        self.applySimulationConditions()
-        self.iteration = 0
-
-        self.timer = QtCore.QTimer(self)
-        self.timerTime = 0
-
-        # a figure instance to plot on
-        self.figure = plt.figure()
-
-        # this is the Canvas Widget that displays the `figure`
-        # it takes the `figure` instance as a parameter to __init__
-        self.canvas = FigureCanvas(self.figure)
-
-        # this is the Navigation widget
-        # it takes the Canvas widget and a parent
-        self.toolbar = NavigationToolbar(self.canvas, self)
-
-        # Just some button connected to `plot` method
-        self.button = QtGui.QPushButton('Start')
-        self.button.clicked.connect(self.start)
-
-        self.pbutton = QtGui.QPushButton('Pause')
-        self.pbutton.clicked.connect(self.stop)
-
-        # set the layout
-        layout = QtGui.QVBoxLayout()
-        layout.addWidget(self.toolbar)
-        layout.addWidget(self.canvas)
-        layout.addWidget(self.button)
-        layout.addWidget(self.pbutton)
-        self.setLayout(layout)
-
-
-        #Calculate dx, dy
-        self.dx = 1./self.sample.shape[0]
-        self.dy = 1./self.sample.shape[1]
+               
+        
+        self.ui = np.zeros((materials.shape)) # matriz inicial de temp
+        self.u = np.zeros((materials.shape)) # matriz inicial de temp
+        self.applySimulationConditions() 
+        
+                  #Calculate dx, dy
+        self.dx = 1./materials.shape[0]
+        self.dy = 1./materials.shape[1]
         self.dx2=self.dx**2 # To save CPU cycles, we'll compute Delta x^2
         self.dy2=self.dy**2 # and Delta y^2 only once and store them.
         print "dx2=", self.dx2
         print "dy2=", self.dy2
-
         # For stability, this is the largest interval possible
         # for the size of the time-step:
-        self.dt = self.dx2*self.dy2/(self.material.conductRock**(self.dx2+self.dy2) )
-        self.show()
-        self.exec_()
-
+        self.dt = self.dx2*self.dy2/(7.8**(self.dx2+self.dy2) )
 
     def applySimulationConditions(self, ambient=20, applied=100):
         """"Set the temperatures for the simulation """
         self.ui.fill(ambient)    #temperatura ambiente
         self.ui[-10:, :] = applied #temperatura aplicada
-        print "Applyed temperature over asphalt:", applied
+        print "Applied temperature over asphalt:", applied
         print "Ambient temperature:", ambient
-
-    def start(self):
-        """Begin the simulation"""
-        self.timer.start(150)
-        QtCore.QObject.connect(self.timer,QtCore.SIGNAL("timeout()"), self.updatefig)
-
-    def stop(self):
-        """Pause the current simulation"""
-        self.timer.stop()
 
     def get_a(self, i, j):
         """Method to get an specific index of the thermal coeficients"""
-        return self.thermicalConstantsMatrix[i, j]
+        return 0.75
 
     def evolve_ts(self, u, ui):
         """
@@ -109,30 +57,13 @@ class ThermalModel(QtGui.QDialog):
                 u[i,j] = ui[i,j]+self.dt*a*(uxx+uyy)
         return ui
 
-    def updatefig(self, animated):
+    def simulate(self, steps=500):
         """"Show the evolution of the thermical simulation"""
-        if animated:
+        for x in range(0, steps):
             self.ui = self.evolve_ts(self.u, self.ui)
     
             self.ui = sp.copy(self.u)
-            self.iteration+=1
-            self.showfig()
-            print "Computing and rendering thermal diffussion step", self.iteration
-        
-    def showfig(self):
-        self.figure.clf()
-        self.figure.add_subplot(111)
-        plt.hold(False)
-        plt.imshow(self.ui, cmap=cm.seismic , interpolation='nearest', origin='lower')
-        plt.colorbar()
-
-        self.figure.add_subplot(121)
-        plt.imshow(self.sample, cmap=cm.seismic , interpolation='nearest', origin='lower')
-        plt.colorbar()
-
-        self.canvas.draw()
-
-    def closeEvent(self, evnt):
-        """Interrupt the execution of the simulation when the dialog is closed"""
-        self.stop()
-
+        print "Done!"
+        plt.imshow(self.u)
+        plt.set_cmap('hot')
+        plt.show()
