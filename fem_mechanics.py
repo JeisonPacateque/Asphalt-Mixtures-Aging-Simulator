773dec37bc8b2b6b1c56beae9665b5af21f5c91f
Jeison Pacateque <sjdps001@gmail.com>
2015-02-02 12:52:10 -0500

    fem_mechanics rectored according to the model, it is neccesary to work more on this class, it is no yet full OOP

--- fem_mechanics.py
+++ fem_mechanics.py
@@ -7,8 +7,6 @@ Created on Tue Nov 25 09:31:31 2014
 
 import numpy as np
 import time
-from matplotlib import pyplot, colors
-
 
 class FEMMechanics(object):
     def __init__(self, matrix_materials):
@@ -18,16 +16,29 @@ class FEMMechanics(object):
         """
         self.MM = matrix_materials # local reference of the matrix materials
 
-        self.ki = np.zeros((self.MM.size)) # Create stiffness matrix
+#==============================================================================
+#        Create stiffness matrix
+#==============================================================================
+        self.ki = np.zeros((self.MM.size))
         cont = 0
         for m in np.nditer(self.MM):
             self.ki[cont] = self.LinearBarElementStiffness(m.young_modulus,\
             m.areaFE, m.lengthFE)
             cont += 1
 
+#==============================================================================
+#       Create conectivity matrix
+#==============================================================================
         self.elements_nodes = []  # Tupla de nodos de cada elemento
         self.elements_top = [] # Indice Elemento superior
         self.elements_bottom = [] #Indice Elemento inferior
+        self.conectivity_matrix = self.ElementConectivityMatrix(self.MM.shape[0],
+                                                           self.MM.shape[1])
+
+        self.K = np.zeros((4600, 4600)) # estos valores hay que cambiarlos, calcularlos dependiendo
+        self.generalStiffnessMatrixAssemble()
+
+        self.force = 0  #applied force over asphalt mixture
 
     def LinearBarElementForces(self, k, u):
         """This function returns the element nodalforce vector given the
@@ -42,86 +53,16 @@ class FEMMechanics(object):
         y = np.dot(k, u)
         return y / A
 
-    def simulate(self):
-        print "Running mechanic simulation..."
-        start_time = time.time()  # Measures Stiffness Assemble matrix time
-
-        # Carga del Slice-----------------------------------------------
-        sample = self.material.vertical_slice
-        slice_size = sample.shape
-
-        #Obtencion de los nodos superiores e inferiores--------------------
-        top_nodes = self.conectivity.getTopElementNodes()
-        bottom_nodes = self.conectivity.getBottomElementNodes()
-
-        self.ki = self.material.ki
-        self.K = self.material.K
-
-        mask = np.ones(self.K.shape[0], dtype=bool)
-        mask[bottom_nodes] = False
-        k_sub = self.K[mask]
-        k_sub = k_sub[:, mask]
-
-        #Aplicar fuerzas sobre el modelo------------------------------------
-        force = 800
-        Fuerzas = force * np.ones(k_sub.shape[0])
-
-        #Calcular desplazamientos-------------------------------------------
-        U = np.linalg.solve(k_sub, Fuerzas)
-
-        #np.set_printoptions(suppress=True)
-        img = U.reshape(slice_size).transpose()
-
-        #Guardar desplazamientos--------------------------------------------
-        # turn off summarization, line-wrapping
-        np.set_printoptions(threshold=np.inf, linewidth=np.inf)
-        with open('matriz_u.txt', 'w') as f:
-            f.write(np.array2string(img, separator=', '))
-
-        #Mostrar desplazamientos--------------------------------------------
-        f = pyplot.figure()
-        f.add_subplot(111)
-        pyplot.title('Displacements Map')
-        pyplot.imshow(img, interpolation='nearest')
-        pyplot.colorbar()
-
-        f.add_subplot(121)
-        pyplot.title('Original Slice')
-        pyplot.imshow(sample.transpose(), interpolation='nearest')
-        pyplot.colorbar()
-        pyplot.show()
-
-        end_time = time.time()  # Get the time when method ends
-        print "Displacements done and saved in",
-        str(end_time - start_time), "seconds."
-
-        print "Top nodes:", len(top_nodes)
-        print "Bottom nodes:", len(bottom_nodes)
-
     def LinearBarElementStiffness(self, E, A, L):
         """ This function returns the element stiffness"""
         return np.array([[E*(A/L), -E*(A/L)], [-E*(A/L) ,E*(A/L)]])
 
-    def generalStiffnessMatrixAssemble(self, slice_size):
+    def generalStiffnessMatrixAssemble(self):
         """Assembles an n x n Stiffness Matrix"""
-        start_time = time.time()  # Measures Stiffness Assemble matrix time
-        #Creacion de la matriz de conectividad------------------------
-
-        con_mtrx = self.conectivity.ElementConectivityMatrix(slice_size[0],
-                                                             slice_size[1])
-        self.K = np.zeros((4600, 4600))
-
         cont = 0
-        for y in con_mtrx:
-            self.K = self.LinearBarAssemble(self.K, self.ki[cont], y[0], y[1])
-            cont = cont+1
-
-#        print "General Stiffness Matriz shape:", self.K.shape
-
-        end_time = time.time()  # Get the time when method ends
-        print "General Stiffness Matriz done in",
-        str(end_time - start_time), "seconds."
-        return self.K
+        for (x, y) in self.conectivity_matrix:
+            self.K = self.LinearBarAssemble(self.K, self.ki[cont], x, y)
+            cont += 1
 
     def LinearBarAssemble(self, K, k, i, j):
         """This function assembles the element stiffness matrix k of the linear
@@ -137,12 +78,11 @@ class FEMMechanics(object):
     def ElementConectivityMatrix(self, width, height):
         """Create the nodes and set positions for all elements
         on a stiffness matrix"""
-
         a = 0
         b = 1
-        for i in range(width):
+        for i in xrange(width):
             self.elements_top.append(a)
-            for j in range(height):
+            for j in xrange(height):
                 self.elements_nodes.append((a, b))
                 a=a+1
                 b=b+1
@@ -150,8 +90,6 @@ class FEMMechanics(object):
             a=a+1
             b=b+1
 
-        print "Conectivity matrix done in",
-        str(end_time - start_time), "seconds."
         return self.elements_nodes
 
     def getTopElementNodes(self):
@@ -159,3 +97,31 @@ class FEMMechanics(object):
 
     def getBottomElementNodes(self):
         return self.elements_bottom
+
+    def applySimulationConditions(self, force=800):
+        self.force = 800
+
+    def simulate(self):
+        # Obtencion de los nodos superiores e inferiores
+        top_nodes = self.getTopElementNodes()
+        bottom_nodes = self.getBottomElementNodes()
+
+        mask = np.ones(self.K.shape[0], dtype=bool)
+        mask[bottom_nodes] = False
+        k_sub = self.K[mask]
+        k_sub = k_sub[:, mask]
+
+        #Apply forces over model------------------------------------
+        forces = self.force * np.ones(k_sub.shape[0])
+
+        #calculate displacements-------------------------------------------
+        U = np.linalg.solve(k_sub, forces)
+
+        #np.set_printoptions(suppress=True)
+        img = U.reshape(self.MM.shape)
+
+        #Guardar desplazamientos--------------------------------------------
+        # turn off summarization, line-wrapping
+        np.set_printoptions(threshold=np.inf, linewidth=np.inf)
+        with open('matriz_u.txt', 'w') as f:
+            f.write(np.array2string(img, separator=', '))
